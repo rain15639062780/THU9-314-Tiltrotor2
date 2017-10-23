0d4714fb47882894475daa5f87cbc77ec235f728
xj-zhang <xj-zhang@xjzhang-310>
2017-10-23 21:50:02 +0800

    修改水平飞行时日志记录方法20171023

--- src/modules/sdlog2/sdlog2_old.c
+++ src/modules/sdlog2/sdlog2_old.c
@@ -187,8 +187,6 @@ static bool log_name_timestamp = false;
 /* helper flag to track system state changes */
 static bool flag_system_armed = false;
 
-static bool is_rw_mode = true;
-
 /* flag if warning about MicroSD card being almost full has already been sent */
 static bool space_warning_sent = false;
 
@@ -1727,9 +1725,6 @@ int sdlog2_thread_main(int argc, char *argv[])
 				log_msg.body.log_VTOL.rw_mode = buf.vtol_status.vtol_in_rw_mode;
 				log_msg.body.log_VTOL.trans_mode = buf.vtol_status.vtol_in_trans_mode;
 				log_msg.body.log_VTOL.failsafe_mode = buf.vtol_status.vtol_transition_failsafe;
-
-				is_rw_mode = buf.vtol_status.vtol_in_rw_mode;
-				
 				LOGBUFFER_WRITE_AND_COUNT(VTOL);
 			}
 
@@ -2279,39 +2274,6 @@ int sdlog2_thread_main(int argc, char *argv[])
 			log_msg.body.log_ATT.roll_rate = buf.att.rollspeed;
 			log_msg.body.log_ATT.pitch_rate = buf.att.pitchspeed;
 			log_msg.body.log_ATT.yaw_rate = buf.att.yawspeed;
-
-			if ( !is_rw_mode )
-			{
-				//math::Quaternion q_att(q0, q1, q2, q3);
-				//math::Matrix<3, 3> _R = q_att.to_dcm();
-				float R_H[3][3];
-				R_H[0][0] = -2.0f*(q0*q2 + q1*q3);
-				R_H[0][1] = 2.0f*(q1*q2-q0*q3);
-				R_H[0][2] = q0*q0+q1*q1+q2*q2+q3*q3;
-				R_H[1][0] = -2.0f*(q2*q3-q0*q1);
-				R_H[1][1] = q0*q0-q1*q1+q2*q2-q3*q3;
-				R_H[1][2] = 2.0f*(q1*q2+q0*q3);
-				R_H[2][0] = -(q0*q0-q1*q1-q2*q2+q3*q3);
-				R_H[2][1] = 2.0f*(q0*q1+q2*q3);
-				R_H[2][2] = 2.0f*(q1*q3-q0*q2);
-				
-				log_msg.body.log_ATT.pitch = asinf(-R_H[2][0]);
-
-				if (fabsf(log_msg.body.log_ATT.pitch - M_PI_2_F) < 1.0e-3f) {
-					log_msg.body.log_ATT.roll = 0.0f;
-					log_msg.body.log_ATT.yaw = atan2f(R_H[1][2] - R_H[0][1], R_H[0][2] + R_H[1][1]) + log_msg.body.log_ATT.roll;
-
-				} else if (fabsf(log_msg.body.log_ATT.pitch + M_PI_2_F) < 1.0e-3f) {
-					log_msg.body.log_ATT.roll = 0.0f;
-					log_msg.body.log_ATT.yaw = atan2f(R_H[1][2] - R_H[0][1], R_H[0][2] + R_H[1][1]) - log_msg.body.log_ATT.roll;
-
-				} else {
-					log_msg.body.log_ATT.roll = atan2f(R_H[2][1], R_H[2][2]);
-					log_msg.body.log_ATT.yaw = atan2f(R_H[1][0], R_H[0][0]);
-				}
-			}
-
-
 			LOGBUFFER_WRITE_AND_COUNT(ATT);
 		}
 
